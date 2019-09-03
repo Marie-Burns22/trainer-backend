dce91bd6ea59f49c84357b519789785204899e24
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-03 11:44:42 -0700

    Update validation for request method in service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,5 +2,6 @@ class Service < ApplicationRecord
     has_many :clients
 
     validates :name, presence: true
+    validates_inclusion_of :request_method, :in => ['Email us', 'Book', 'Request' ]
 
 end
