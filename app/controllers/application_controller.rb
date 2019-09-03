49484214f742ed3cadf5fcc6e511380d555d1337
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-03 16:07:47 -0700

    Begin setup of redux

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+        def current_client
+        # mock hard coded version. Fix once there are more users.
+            Client.first
+        end
+
+        def logged_in?
+            !!current_client
+        end
+
 end
