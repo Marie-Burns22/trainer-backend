5c7db5814ef9d71d6ad5f05bb1180fadeaf0bd78
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-09-11 11:31:41 -0700

    Add new sessions and remove old

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,21 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
-    def create
-        # @client = Client.find_by(email: params[:client][:email])
 
-        # if @client && @client.authenticate(params[:client][:password])
-        #     session[:client_id] = @client.id 
-        #     render json: @client 
-        # else 
-        #     render json: {
-        #         error: "Invalid Credentials"
-        #     }, state: unauthorized 
-        # end
+    def create
     end
 
-    def destroy
-        # session.clear
-        # render json: {
-        #     message: "Logged out"
-        # }
+    def delete
+        
     end
 end
